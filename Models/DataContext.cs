7f8b88f7e8aa96d5bbb05b307175ea8ae172faa7
shaik zuhair ahamed <zuhairahamed3114@gmail.com>
2021-07-31 23:09:29 +0530

    adding the Models

---
+++ Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProBlazorNew.Models
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opts):base(opts) { 
+        }
+
+        public DbSet<Person> Peoples { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProBlazorNew.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public long DepartmentId { get; set; }
+        public long LocationId { get; set; }
+        public Department Department { get; set; }
+        public Location Location { get; set; } 
+
+        
+    }
+}
